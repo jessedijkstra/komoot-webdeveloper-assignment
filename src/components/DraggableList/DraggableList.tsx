ce428e7d62a12e8bef719b2b5c15be7b62ff4d17
Jesse Dijkstra <mail@jessedijkstra.nl>
2023-11-01 15:18:33 +0100

    Move DraggableListItem to own component

--- src/components/DraggableList/DraggableList.tsx
+++ src/components/DraggableList/DraggableList.tsx
@@ -6,21 +6,29 @@ type TDraggableListItem = {
   name: string;
 };
 
-const DraggableList = ({ items }: { items: TDraggableListItem[] }) => {
-  const [list, setList] = useState(items);
+type DraggableListItemProps = React.PropsWithChildren<{
+  index: number;
+  onDragStart: (index: number) => any;
+  onDragOver: (index: number) => any;
+  onDragEnter: (index: number) => any;
+  onDragEnd: (index: number) => any;
+}>;
 
-  const dragFromPosition = useRef(-1);
-  const targetPosition = useRef(-1);
-
-  const onDragStart = (event: React.DragEvent, index: number) => {
-    dragFromPosition.current = index;
+const DraggableListItem = ({
+  children,
+  index,
+  onDragStart,
+  onDragOver,
+  onDragEnter,
+  onDragEnd,
+}: DraggableListItemProps) => {
+  const handleDragStart = (event: React.DragEvent, index: number) => {
     event.currentTarget.classList.add("element-is-being-dragged");
-  };
-  const onDragEnter = (_event: React.DragEvent, index: number) => {
-    targetPosition.current = index;
+
+    onDragStart(index);
   };
 
-  const onDragOver = (event: React.DragEvent, index: number) => {
+  const handleDragOver = (event: React.DragEvent, index: number) => {
     const boundingRectangle = event.currentTarget.getBoundingClientRect();
     const center = boundingRectangle.top + boundingRectangle.height / 2;
 
@@ -28,23 +36,61 @@ const DraggableList = ({ items }: { items: TDraggableListItem[] }) => {
       event.currentTarget.classList.add("element-drags-to-top");
       event.currentTarget.classList.remove("element-drags-to-bottom");
 
-      targetPosition.current = index;
+      onDragOver(index);
     } else {
       event.currentTarget.classList.add("element-drags-to-bottom");
       event.currentTarget.classList.remove("element-drags-to-top");
 
-      targetPosition.current = index + 1;
+      onDragOver(index + 1);
     }
   };
 
-  const onDragLeave = (event: React.DragEvent) => {
+  const handleDragLeave = (event: React.DragEvent) => {
     event.currentTarget.classList.remove("element-drags-to-top");
     event.currentTarget.classList.remove("element-drags-to-bottom");
   };
 
-  const onDragEnd = (event: React.DragEvent) => {
-    const currentItem = list[dragFromPosition.current];
+  const handleDragEnd = (event: React.DragEvent) => {
     event.currentTarget.classList.remove("element-is-being-dragged");
+
+    onDragEnd(index);
+  };
+
+  return (
+    <li
+      onDragStart={(event) => handleDragStart(event, index)}
+      onDragEnter={(_event) => onDragEnter(index)}
+      onDragEnd={handleDragEnd}
+      onDragLeave={handleDragLeave}
+      onDragOver={(event) => handleDragOver(event, index)}
+      draggable
+      className="DraggableListItem"
+    >
+      {children}
+    </li>
+  );
+};
+
+const DraggableList = ({ items }: { items: TDraggableListItem[] }) => {
+  const [list, setList] = useState(items);
+
+  const dragFromPosition = useRef(-1);
+  const targetPosition = useRef(-1);
+
+  const onDragStart = (index: number) => {
+    dragFromPosition.current = index;
+  };
+
+  const onDragEnter = (index: number) => {
+    targetPosition.current = index;
+  };
+
+  const onDragOver = (index: number) => {
+    targetPosition.current = index;
+  };
+
+  const onDragEnd = () => {
+    const currentItem = list[dragFromPosition.current];
 
     if (currentItem) {
       const newList = [
@@ -64,18 +110,16 @@ const DraggableList = ({ items }: { items: TDraggableListItem[] }) => {
   return (
     <ol className="DraggableList" data-testid="DraggableList">
       {list.map(({ name, id }, index) => (
-        <li
+        <DraggableListItem
           key={id}
-          onDragStart={(event) => onDragStart(event, index)}
-          onDragEnter={(event) => onDragEnter(event, index)}
+          index={index}
+          onDragStart={onDragStart}
+          onDragEnter={onDragEnter}
           onDragEnd={onDragEnd}
-          onDragLeave={onDragLeave}
-          onDragOver={(event) => onDragOver(event, index)}
-          draggable
-          className="DraggableListItem"
+          onDragOver={onDragOver}
         >
           {name}
-        </li>
+        </DraggableListItem>
       ))}
     </ol>
   );
